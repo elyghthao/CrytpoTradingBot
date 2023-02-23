9c3c74f6cf74514bdf1f99941590c601b617441e
elyghthao <elyghthao@gmail.com>
2023-02-22 17:22:30 -0800

    f

--- KrakenCryptoBot.py
+++ KrakenCryptoBot.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from termcolor import colored
 import os
 import json
+import traceback
 
 import datetime
 
@@ -45,14 +46,21 @@ def isInOpenOrder(curCoin):
 
 
 
-percentageChange = 1.5  # in percentage form
+percentageChange = 1.2  # in percentage form
 percentageToUse = 80  # in percentage form
 flatPriceToUse = 0  # amount of dollars to use, is in dollars, default to 0 to use percentage
 count = 0
-coinTime = 2 #amount of seconds between each query
-iterationTime = 60 #amount of seconds between each iteration (while loop)
+coinTime = 0.5 #amount of seconds between each query
+iterationTime = 5 #amount of seconds between each iteration (while loop)
 
-coinList = ["APEUSD", "ADAUSD", "MATICUSD", "ATOMUSD", "TRXUSD", "LDOUSD", "ALGOUSD"]
+# secs = 1800 #30 minutes ago
+secs = 2700 #45 minutes ago
+# secs = 3600 #60 minutes ago
+timeInterval = int(time.time()) - secs
+
+
+coinList = ["APEUSD", "ADAUSD", "MATICUSD", "ATOMUSD", "TRXUSD", "LDOUSD", "ALGOUSD", "MANAUSD", "SANDUSD", "LRCUSD", "SUSHIUSD", "OCEANUSD", "SHIBUSD"]
+# coinList = ["APEUSD", "ADAUSD", "MATICUSD", "ATOMUSD", "TRXUSD", "LDOUSD", "ALGOUSD"]
 # coinList = ["ADAUSD"]
 
 
@@ -67,9 +75,13 @@ time.sleep(coinTime)
 while (True):
 
     try:
+
         print("count: " + str(count))
         count += 1
+        
 
+
+        print("interval: " + str(secs) + " seconds")
         #go through currentOrderList and see if they are all open, if closed, remove it and put into receipt
         print("current order list: " + str(currentOrderIdList))
         for order_Id in currentOrderIdList:
@@ -94,6 +106,7 @@ while (True):
 
         for pair in coinList:
             time.sleep(coinTime)#-------------------------------
+            print()
             #Current Balance
             balance = kraken_request("/0/private/Balance", {
                 "nonce": str(int(1000 * time.time()))
@@ -116,10 +129,13 @@ while (True):
 
 
             # Get the last 30 minutes worth of OHLC data
-            timestamp_30_minutes_ago = int(time.time()) - 1800
-            test = k.query_public('Trades', {'pair': pair, 'since': timestamp_30_minutes_ago})
-            past_price =float( test['result'][pair][0][0])
+            
+
+            past = k.query_public('Trades', {'pair': pair, 'since': timeInterval})
+            past_price =float( past['result'][pair][0][0])
             # print("pastPrice: " + str(past_price))
+            time.sleep(coinTime)#-------------------------------
+
             current_price = float(k.query_public('Ticker', {'pair': pair})['result'][pair]['c'][0])
             # print("currentPrice: " + str(current_price))
             sell_price = round(float(current_price + (past_price - current_price)/3), decimalPlaces) #sell_price percentage: currently is 1/3 the percentage change (mean calculation)
@@ -148,8 +164,8 @@ while (True):
             # print("ordermin: " + str(orderMin))
 
 
-            print(colored(pair + " USD: $" + str(useAmount), 'magenta'))
-            print(colored (str(past_price) + " --> " + str(current_price) + "   (" + str(price_change) + "%)", 'blue'))
+            print(colored(pair + " (USD AVAILABLE: $" + str(useAmount) + ")",'light_magenta'))
+            print(colored (str(past_price) + " --> " + str(current_price) + "   (" + str(price_change) + "%)", 'cyan'))
             if(isInOpenOrder(pair)):
                 print(colored ("is already in open order", 'yellow'))
                 continue
@@ -162,11 +178,11 @@ while (True):
 
             # if price change is large enough, do a market buy, and limit sell with price sell_price 
             if price_change < -percentageChange:
-                print("openPrice: " + str(past_price))
-                print("closePrice: " + str(current_price))
+                # print("openPrice: " + str(past_price))
+                # print("closePrice: " + str(current_price))
                 # print((ohlc['result'][pair]))
                 print(
-                    colored("The price of " + pair + " has dropped by more than " + str(percentageChange) + "% in the last 30 minutes  MAKE ORDER", 'green'))
+                    colored("The price of " + pair + " has dropped by more than " + str(percentageChange) + "% in the last 30 minutes  MAKE ORDER", 'light_green'))
                 buyOrder = kraken_request("/0/private/AddOrder", {
                     "nonce": str(int(1000 * time.time())),
                     "ordertype": "market",
@@ -174,7 +190,7 @@ while (True):
                     "volume" : amountOfVolume,
                     "pair": pair,
                 }, api_key, private_key)
-                print(colored(buyOrder.json(), 'green'))
+                print(colored(buyOrder.json(), 'light_green'))
                 time.sleep(coinTime)#-------------------------------
 
                 sellOrder = kraken_request("/0/private/AddOrder", {
@@ -185,29 +201,30 @@ while (True):
                     "pair": pair,
                     "price": sell_price,
                 }, api_key, private_key)
-                print(colored(sellOrder.json(), 'green'))
+                print(colored(sellOrder.json(), 'light_green'))
                 currentOrderIdList.append(sellOrder.json()['result']['txid'][0])
                 time.sleep(coinTime)#-------------------------------
 
             #    message describing buy and sell process, spits out info into receipt.txt
                 message = str(datetime.datetime.now()) + "\t(BOUGHT " + str(useAmount) + " " + pair + " for " + "$" + str(round(current_price,2)) + ")  (SELL AT $" + str(sell_price) + ")" + "  (BALANCE  Before: $" + str(amountOfCash) + " Now: $" + str(amountOfCash - useAmount) + ")"
-                print(colored (message, 'green'))
+                print(colored (message, 'light_green'))
                 with open('receipt.txt', 'a') as f:
                     f.write(message + "\n")
                 f.close()
             else:
-                print(colored ("The price of " + str(pair) + " has not dropped by more than " + str(percentageChange) + "% in the last 30 minutes", 'yellow'))
+                print(colored ("The price of " + str(pair) + " has not dropped by more than " + str(percentageChange) + "% in the chosen interval", 'yellow'))
             
 
         print(balance)
-        print(colored("________________________________________________________________________________________________________________________________", 'blue'))
-         # testing for now as 15
+        print(colored("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*",'blue'))
+         # pasting for now as 15
     except Exception as e: 
-        print(colored("ERROR " + e, 'red'))
-        message = str(datetime.datetime.now()) + "\t(ERROR) " + e
+        lineError = traceback.extract_tb(e.__traceback__)[-1][1]
+        print( colored ("error on line " + str(lineError) + ": " + str(e), 'red'))
+        message = str(datetime.datetime.now()) + "\t(ERROR on line " + str(lineError) + " and count " + str(count) + ") :" + str(e)
         with open('receipt.txt', 'a') as f:
             f.write(message + "\n")
             f.close()
-        time.sleep(iterationTime * 2)
+        time.sleep(iterationTime)
 
     time.sleep(iterationTime) 
